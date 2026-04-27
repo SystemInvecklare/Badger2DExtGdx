c8dc694f7089054d9551e5f08fc4499264d939b9
Mattias Selin <mattias.selin@gmail.com>
2026-04-27 16:24:42 +0200

    Made texture saving gwt-compliable

--- src/main/java/com/github/systeminvecklare/badger/impl/gdx/store/AtlasStore.java
+++ src/main/java/com/github/systeminvecklare/badger/impl/gdx/store/AtlasStore.java
@@ -8,27 +8,23 @@ import java.util.Map.Entry;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.glutils.FrameBuffer;
-import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.JsonWriter;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.github.systeminvecklare.badger.core.math.Mathf;
 import com.github.systeminvecklare.badger.impl.gdx.FlashyGdxEngine;
 import com.github.systeminvecklare.badger.impl.gdx.store.atlas.IAtlasBuilder;
 import com.github.systeminvecklare.badger.impl.gdx.store.atlas.IAtlasConstruction;
 import com.github.systeminvecklare.badger.impl.gdx.store.atlas.ITextureAtlas;
+import com.github.systeminvecklare.badger.impl.gdx.util.TextureSaver;
 
 public class AtlasStore {
 	private static final TextureWrap DEFAULT_WRAP = TextureWrap.ClampToEdge;
@@ -268,7 +264,7 @@ public class AtlasStore {
 		}
 		
 		private void save(FileHandle path, String imageName, String jsonName, String overflowAtlasJsonNameOrNull) throws IOException {
-			saveTexture(path.child(imageName));
+			TextureSaver.saveTexture(path.child(imageName), texture);
 			
 			FileHandle jsonFilePath = path.child(jsonName);
 			JsonWriter writer = new JsonWriter(jsonFilePath.writer(false, "utf-8"));
@@ -296,51 +292,6 @@ public class AtlasStore {
 			} finally {
 				writer.close();
 			}
-		}
-		
-		private void saveTexture(FileHandle imageFile) {
-		    int w = texture.getWidth();
-		    int h = texture.getHeight();
-
-		    FrameBuffer fbo = new FrameBuffer(Pixmap.Format.RGBA8888, w, h, false);
-		    SpriteBatch batch = new SpriteBatch();
-		    
-		    batch.setBlendFunction(GL20.GL_ONE, GL20.GL_ONE_MINUS_SRC_ALPHA);
-		    
-		    fbo.begin();
-
-		    Gdx.gl.glViewport(0, 0, w, h);
-
-		    Gdx.gl.glClearColor(0, 0, 0, 0);
-		    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		    batch.setProjectionMatrix(new Matrix4().setToOrtho2D(0, 0, w, h));
-
-		    batch.begin();
-
-		    batch.draw(texture, 0, 0, w, h);
-
-		    batch.end();
-
-		    Pixmap pixmap = ScreenUtils.getFrameBufferPixmap(0, 0, w, h);
-
-		    fbo.end();
-
-		    Pixmap flipped = new Pixmap(w, h, pixmap.getFormat());
-
-		    for (int y = 0; y < h; y++) {
-		        for (int x = 0; x < w; x++) {
-		            flipped.drawPixel(x, h - 1 - y, pixmap.getPixel(x, y));
-		        }
-		    }
-
-		    pixmap.dispose();
-
-		    PixmapIO.writePNG(imageFile, flipped);
-		    flipped.dispose();
-
-		    batch.dispose();
-		    fbo.dispose();
 		}
 	}
 	
