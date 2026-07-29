987ba1691188d151517f48cb873792c210e30750
Mattias Selin <mattias.selin@gmail.com>
2026-07-29 11:45:08 +0200

    Added some javadoc

--- src/main/java/com/github/systeminvecklare/badger/impl/gdx/fbo/IFboHandle.java
+++ src/main/java/com/github/systeminvecklare/badger/impl/gdx/fbo/IFboHandle.java
@@ -3,6 +3,19 @@ package com.github.systeminvecklare.badger.impl.gdx.fbo;
 import com.badlogic.gdx.graphics.Color;
 import com.github.systeminvecklare.badger.core.graphics.components.core.IDrawCycle;
 
+/**
+ * Usage:
+ * <br>
+ * <pre>
+ * <code>
+ * if(fboHandle.drawCached(drawCycle)) {
+ *   // Normal drawing here
+ *   fboHandle.done(); // Required
+ * }
+ * </code>
+ * </pre>
+ * @author Mattias Selin
+ */
 public interface IFboHandle {
 	boolean drawCached(IDrawCycle drawCycle);
 	void done();
